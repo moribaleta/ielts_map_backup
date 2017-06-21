d04895b77959d1f2ae7be225c6014ad77c6484b2
Unknown <moribaleta@gmail.com>
2017-06-22 01:30:43 +0800

    updated canada-map.js
    implemented the map function to load when the document is loading

--- canada-map.js
+++ canada-map.js
@@ -10,6 +10,149 @@
 
 // Map for Canada
 var canadaMap;
+function openMap() {
+    var stateNameParser = document.getElementById('state-name-parser');
+    // Load the Canada map
+   canadaMap = new Datamap({
+        element: document.getElementById('canada-map-container'),
+        scope: 'provinces',
+        setProjection: function (element, options) {
+            var projection = d3.geo.mercator()
+            .center([-95, 71])
+            .scale(420)
+            .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+
+            var path = d3.geo.path()
+            .projection(projection);
+
+            return {path: path, projection: projection};
+        },
+        responsive: true,
+        geographyConfig: {
+            dataUrl: '/maps/canada.topo.json',
+            highlightFillColor: 'rgb(227, 24, 55)',
+            popupTemplate: function (geography, data) {
+                var value = geography.properties.name;
+                var output = 0;
+                for(var i=0; i<states.length;i++){
+                    var elem = states[i];
+                    elem = elem.split(",");
+                    if(elem[0]==value){
+
+                        output = elem[1];
+                    }
+                }if(output>0)            
+                return '<div class="hoverinfo">' + geography.properties.name +
+                ', Number of Universities: ' + output + ' '
+            }
+        },
+        // Define the color for states on the map base on the universities numbers
+        fills: {
+            "c0": 'rgba(225,225, 225, 1)', // Grey: 0
+            "c1": '#8DD15A', //green: 1-20
+            "c2": '#AD70D1', //purple: 21-50
+            "c3": '#FC9651', //orange: 51-75
+            "c4": '#8AB3E0', //blue: 76-100
+            "c5": '#FF001E', //red: 101-150
+            "c6": '#248699', //turquoise: 151-200
+            "c7": '#FFCBFD', //pink: over 200
+            defaultFill: 'rgba(225,225, 225, 1)'
+        },
+        data: {
+            // TODO: update universities data and fillKey for each states
+            "Nunavut":{
+                "fillKey": "c0",
+                "state":"Nunavut",
+                "numberOfUniversities": 0
+            },
+            "Quebec":{
+                "fillKey": "c0",
+                "state":"Quebec",
+                "numberOfUniversities": 9
+            },
+            "Northwest Territories":{
+                "fillKey": "c0",
+                "state":"Northwest Territories",
+                "numberOfUniversities": 0
+            },
+            "Ontario":{
+                "fillKey": "c0",
+                "state":"Ontario",
+                "numberOfUniversities": 0
+            },
+            "Alberta":{
+                "fillKey": "c0",
+                "state":"Alberta",
+                "numberOfUniversities": 0
+            },
+            "British Columbia":{
+                "fillKey": "c0",
+                "state":"British Columbia",
+                "numberOfUniversities": 0
+            },
+            "Manitoba":{
+                "fillKey": "c0",
+                "state":"Manitoba",
+                "numberOfUniversities": 0
+            },
+            "Yukon":{
+                "fillKey": "c0",
+                "state":"Yukon",
+                "numberOfUniversities": 0
+            },
+            "Newfoundland and Labrador":{
+                "fillKey": "c0",
+                "state":"Newfoundland and Labrador",
+                "numberOfUniversities": 0
+            },
+            "New Brunswick": {
+                "fillKey": "c0",
+                "state":"New Brunswick",
+                "numberOfUniversities": 0
+            },
+            "Nova Scotia": {
+                "fillKey": "c0",
+                "state":"Nova Scotia",
+                "numberOfUniversities": 0
+            },
+            "Prince Edward Island": {
+                "fillKey": "c0",
+                "state": "Prince Edward Island",
+                "numberOfUniversities": 0
+            },
+            "Saskatchewan": {
+                "fillKey": "c0",
+                "state": "Saskatchewan",               
+                "numberOfUniversities": 0
+            }
+        },
+
+
+        done: function (datamap) {
+            datamap.svg.selectAll('.datamaps-subunit').on('click', function (geography) {
+                // Pass the selected state name to state-name-parser input
+                if (geography.properties.name != null) {
+                    stateNameParser.value = geography.properties['name'];
+                }
+                console.log(stateNameParser.value + ": " + geography.properties['name']);
+            });
+            datamap.svg.selectAll('.datamaps-subunit').on('touchstart', function (geography) {
+                // Pass the selected state name to state-name-parser input
+                if (geography.properties.name != null) {
+                    stateNameParser.value = geography.properties['name'];
+                }
+            });
+            console.log("Object o", this.data);
+        }
+    });
+
+    // Responsive Map
+        window.addEventListener('resize', function () {
+            canadaMap.resize();
+        });
+    }
+    openMap();
+/*    
 (function () {
     var stateNameParser = document.getElementById('state-name-parser');
     // Load the Canada map
@@ -201,7 +344,7 @@ var canadaMap;
 
     }*/
 
-})();
+//})();
 
 /*
 document.onreadystatechange = function () {
